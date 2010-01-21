2c3db439456109ecc7a0f24c8dddc7aec56e0c7c
Andy Weinberg <andy.weinberg@asurion.com>
2010-01-21 10:42:05 -0600

    Vmware CD Iso scripts v1

---
+++ VC-iso-unmount.ps1
@@ -0,0 +1,20 @@
+﻿#******************************************************
+#
+# VMware Host CDrom ISO unmount Script
+#
+# Written By: Andy Weinberg
+#
+# Version 1.0, 1/7/2010
+#
+#****************************************************
+
+
+Add-PSSnapin VMware.VimAutomation.Core
+
+Connect-VIServer vcusa
+
+$vms = Get-VM
+
+$CDConnected =  Get-CDDrive $vms | where {$_.RemoteDevice -like "*.ISO*"}
+
+Set-CDDrive -Connected 0 $CDConnected
---
+++ umountiso-cluster.ps1
@@ -0,0 +1,36 @@
+﻿###########################################
+#
+# VMware cluster CD disconenct script
+#
+# Written By: Andy.Weinberg
+#
+# Version 1.0, 1/20/2010
+#
+###########################################
+
+Add-PSSnapin VMware.VimAutomation.Core
+
+# Connect to VI server
+$vserver = Read-Host -Prompt "What VI server do you want to connect to?"
+Connect-VIServer $vserver 
+
+#Get clusters on the server
+$cluster = Get-Cluster
+
+# User to enter what cluster they want to work with
+
+$cluster
+$cluster = Read-Host -Prompt "Please Enter the Cluster Name"
+
+# Get the VMhosts of the cluster
+
+$vms = Get-Cluster -Name $cluster | Get-VM
+
+del variable:\cluster
+del variable:\vserver
+
+# Determine if CD is mounted and disconnect them
+$CDConnected = Get-CDDrive $vms | where {$_.RemoteDevice -like "*.ISO*"}
+Set-CDDrive -NoMedia $CDConnected -Confirm
+
+
